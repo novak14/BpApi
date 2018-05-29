9f20efbe24cf798bacd276f85dbb24bd3388d391
Marcel Novák <marcel.novak14@live.com>
2018-05-29 17:52:13 +0200

    First

--- BpApi/Controllers/ValuesController.cs
+++ BpApi/Controllers/ValuesController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using BpApi.Models;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BpApi.Controllers
@@ -13,7 +17,18 @@ namespace BpApi.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            string sql = @"SELECT * FROM UserDetails";
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            using (var connection = new SqlConnection("Data Source=blue.globenet.cz;Initial Catalog=d001420;Integrated Security=False;User ID=d001420a;Password=snbx8DkjUE;Connect Timeout=15;Encrypt=False;Packet Size=4096"))
+            {
+                var tmp = connection.Query<UserInformation>(sql);
+                //dom = connection.Query<TestModel>(sql);
+            }
+            stopwatch.Stop();
+            var testStopwatch = stopwatch.Elapsed.ToString();
+            return new string[] { testStopwatch, "value2" };
         }
 
         // GET api/values/5
